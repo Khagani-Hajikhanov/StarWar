e8f0aaba658ff678bdbbbb3fd7bbe79d8c39377f
Khagani-Hajikhanov <dark_aze10@mail.ru>
2021-04-28 03:14:49 +0400

    last change

--- js/java.js
+++ js/java.js
@@ -1,7 +1,23 @@
 var userName = "";
-var selectHero = "";
-var secildi = 0;
-var secildi2=0;
+var selectHero = 0;
+var secildi = 1;
+var secildi2 = 1;
+var userLife = 1000;
+var computerLife = 1000;
+var attackScore = 0;
+var lvl =1;
+var computerRandom = 400;
+
+            var bgSound = document.getElementById("bg");
+            function playAudio() { 
+                bgSound.play(); 
+            } 
+            playAudio();
+
+document.querySelector('.solHealthCount').innerHTML = userLife;
+document.querySelector('.sagHealthCount').innerHTML = computerLife;
+
+
 document.querySelector(".delete").onclick = function(){
     userName = "";
     document.querySelector('.userlogininput').innerHTML = "_";
@@ -25,44 +41,80 @@ window.onkeypress = function(e){
         console.log(e.key);
     }
 }
-    document.getElementById("h"+1).onclick = function(){
+
+
+    document.getElementById("h1").onclick = function(){
         secildi2=secildi;
-        secildi = parseInt(document.getElementById("h"+1).dataset.hero);
-        seciliHeroIcon(secildi);
+        secildi = parseInt(document.getElementById("h1").dataset.hero);
+        seciliHeroIcon();
     };
 
     document.getElementById("h2").onclick = function(){
         secildi2=secildi;
         secildi = parseInt(document.getElementById("h2").dataset.hero);
-        seciliHeroIcon(secildi);
+        seciliHeroIcon();
+        
     };
 
     document.getElementById("h3").onclick = function(){
         secildi2=secildi;
         secildi = parseInt(document.getElementById("h3").dataset.hero);
-        seciliHeroIcon(secildi);
+        seciliHeroIcon();
     };
 
     document.getElementById("h4").onclick = function(){
         secildi2=secildi;
         secildi = parseInt(document.getElementById("h4").dataset.hero);
-        seciliHeroIcon(secildi);
+        seciliHeroIcon();
     };
 
-    function seciliHeroIcon(z){
-        document.getElementById("h"+z).style.boxShadow = "1px 0px 10px 1px red";
+
+
+    function seciliHeroIcon(){
+        console.log(secildi);
         document.getElementById("h"+secildi2).style.boxShadow = "0px 0px 0px 0px white";
+        document.getElementById("h"+secildi).style.boxShadow = "1px 0px 10px 1px red";
     }
 
     document.querySelector('.userstartbtn').onclick = function(){
         if(userName.length >=3 && secildi != 0){
-            alert("Hersey duzdu");
+
+            if(userName == "ELSHAD"){
+                alert("Xos gelmisiniz " +userName +" muellim");
+            }else{
+                alert("Xos gelmisiniz " +userName);
+            }
+
+            
             document.querySelector('.startcont').style.display = "none";
+            anaSehifeStart();
         }else{
-            if(userName < 3){
+            if(userName.length < 3){
                 alert("IStifadeci adi minimum 3 simvol olmalidir");
-            }else if(secildi === 0){
+            }
+            else if(secildi === 0){
                 alert("Select your hero");
             }
         }
     }
+
+function anaSehifeStart(){
+    document.querySelector('.solUserName').innerHTML = userName;
+    console.log(secildi);
+    switch(secildi){
+        case 1:
+            document.querySelector('.homePageImage').src = "../image/hero1.jpg";
+        break;
+        case 2:
+            document.querySelector('.homePageImage').src = "../image/hero2.jpg";
+        break;
+        case 3:
+            document.querySelector('.homePageImage').src = "../image/hero3.png";
+        break;
+        case 4:
+            document.querySelector('.homePageImage').src = "../image/hero 4.jpg";
+        break;
+    }
+    
+}
+
--- js/java2.js
+++ js/java2.js
@@ -9,8 +9,53 @@ document.querySelector('.pause').onclick = function(){
         setTimeout(function() { 
             var c = document.querySelector('.range').getBoundingClientRect().x
             b = document.querySelector('.attack').getBoundingClientRect().x;
-            var son = b-c;
-            document.querySelector('.netice').innerHTML = Math.ceil(son);
+            var zz = Math.ceil(b-c);
+            if( zz > 200){
+                attackScore = 400-zz;
+            }else{
+                attackScore = zz;
+            }
+            
+            var computerScore = Math.ceil(Math.random() * computerRandom);
+            document.querySelector('.scoreUser').innerHTML = attackScore;
+            document.querySelector('.scoreComp').innerHTML = computerScore;
+
+            userLife  -= computerScore;
+            computerLife -=zz;
+            
+
+            att.style.animationPlayState = 'running';
+            
+            if(computerLife < 1 ){
+                var x = document.getElementById("trueAudio");
+                function playAudio() { 
+                x.play(); 
+            } 
+        playAudio();
+                levelUp();
+            }
+            else if(userLife < 1){
+                var x = document.getElementById("falseAudio");
+            function playAudio() { 
+                x.play(); 
+            } 
+            playAudio();
+                levelLose();
+            }
+                document.querySelector('.solHealthCount').innerHTML = userLife;
+                document.querySelector('.sagHealthCount').innerHTML = computerLife;
+
+                var computerMeter2 = document.querySelector('.sagMeter');
+                computerMeter2.setAttribute('max', lvl*1000);
+                computerMeter2.setAttribute('value', computerLife);
+
+                var userMeter2 = document.querySelector('.solMeter');
+                userMeter2.setAttribute('max', lvl*1000);
+                userMeter2.setAttribute('value', userLife);
+
+                
+                
+
          }, 500);
         
     };
@@ -20,3 +65,25 @@ document.querySelector('.pause').onclick = function(){
         
     }
 
+
+    function levelUp(){
+        
+        
+        alert("YOU WIN !!!");
+        lvl ++;
+        document.querySelector('.level').innerHTML = "LEVEL: " +lvl;
+        userLife = lvl *1000;
+        computerLife = lvl *1000;
+                document.querySelector('.scoreUser').innerHTML = "-";
+                document.querySelector('.scoreComp').innerHTML = "-";
+    }
+
+    function levelLose(){
+        
+        
+        alert("YOU LOSE !!!");
+        userLife = lvl *1000;
+        computerLife = lvl *1000;
+                document.querySelector('.scoreUser').innerHTML = "-";
+                document.querySelector('.scoreComp').innerHTML = "-";
+    }
